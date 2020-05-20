6b7a8cd120b56234e619d8754d838b30139468ed
AdrianTJenkins <adrian.jenkins99@gmail.com>
2020-05-20 13:18:10 -0400

    improved solution

--- index.js
+++ index.js
@@ -1,46 +1,47 @@
-var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-
-switch(randomNumber1) {
-  case 1:
-    document.querySelector('.img1').setAttribute('src', 'images/dice1.png');
-    break;
-  case 2:
-    document.querySelector('.img1').setAttribute('src', 'images/dice2.png');
-    break;
-  case 3:
-    document.querySelector('.img1').setAttribute('src', 'images/dice3.png');
-    break;
-  case 4:
-    document.querySelector('.img1').setAttribute('src', 'images/dice4.png');
-    break;
-  case 5:
-    document.querySelector('.img1').setAttribute('src', 'images/dice5.png');
-    break;
-  case 6:
-    document.querySelector('.img1').setAttribute('src', 'images/dice6.png');
-    break;
-  default:
+function roll() {
+  return Math.floor(Math.random() * 6) + 1;
 }
 
-switch(randomNumber2) {
-  case 1:
-    document.querySelector('.img2').setAttribute('src', 'images/dice1.png');
-    break;
-  case 2:
-    document.querySelector('.img2').setAttribute('src', 'images/dice2.png');
-    break;
-  case 3:
-    document.querySelector('.img2').setAttribute('src', 'images/dice3.png');
-    break;
-  case 4:
-    document.querySelector('.img2').setAttribute('src', 'images/dice4.png');
-    break;
-  case 5:
-    document.querySelector('.img2').setAttribute('src', 'images/dice5.png');
-    break;
-  case 6:
-    document.querySelector('.img2').setAttribute('src', 'images/dice6.png');
-    break;
-  default:
+function dice(imageClass, number) {
+  switch(number) {
+    case 1:
+      document.querySelector(imageClass).setAttribute("src", "images/dice1.png");
+      break;
+    case 2:
+      document.querySelector(imageClass).setAttribute("src", "images/dice2.png");
+      break;
+    case 3:
+      document.querySelector(imageClass).setAttribute("src", "images/dice3.png");
+      break;
+    case 4:
+      document.querySelector(imageClass).setAttribute("src", "images/dice4.png");
+      break;
+    case 5:
+      document.querySelector(imageClass).setAttribute("src", "images/dice5.png");
+      break;
+    case 6:
+      document.querySelector(imageClass).setAttribute("src", "images/dice6.png");
+      break;
+    default:
+  }
 }
+
+function winner(num1, num2) {
+  if (num1 > num2) {
+    document.querySelector("h1").textContent = "Player 1 Wins!"
+  }
+  else if (num1 < num2) {
+    document.querySelector("h1").textContent = "Player 2 Wins!"
+  }
+  else {
+    document.querySelector("h1").textContent = "Draw!"
+  }
+}
+
+var randomNumber1 = roll();
+var randomNumber2 = roll();
+
+dice(".img1", randomNumber1);
+dice(".img2", randomNumber2);
+
+winner(randomNumber1, randomNumber2);
